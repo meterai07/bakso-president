45efad5ba981df95b0272f4cdb38826e9e8ad86c
canninoal <canninoalby@gmail.com>
2023-05-26 01:17:31 +0700

    new file:   public/assets/icons/circles.svg 	new file:   public/assets/icons/clock.svg 	new file:   public/assets/icons/phone.svg 	new file:   public/assets/icons/pin.svg 	new file:   public/assets/icons/way.svg 	modified:   resources/views/index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -41,6 +41,7 @@
             <p class="text-base font-normal text-neutral-50 w-[600px]">Lorem ipsum dolor sit amet consectetur. Cursus enim libero mattis lectus vitae convallis nunc dictum fusce. Est eleifend tristique porta magna ut augue ultricies. Arcu sit eu tellus ac elementum. Nulla dictum arcu enim viverra cursus eu parturient congue. Pharetra cursus nibh vitae maecenas iaculis nunc consectetur ultrices.</p>
         </div>
     </div>
+    <!-- Best Seller -->
     <div class="flex flex-col items-center justify-center gap-6">
         <h1 class="font-lora font-semibold text-2xl text-neutral-900">Menu</h1>
         <h2 class="font-lora font-bold text-4xl ">Our <span class="text-primary-500">Best Seller</span></h2>
@@ -92,13 +93,98 @@
                 <img src="..\assets\img\Button 1.svg" alt="">
             </button>
         </div>
-        <button class="flex flex-row justify-center items-center px-3 pt-4 pb-6 gap-2 bg-neutral-50 border-solid border-primary-500  rounded-lg">
+        <button class="flex gap-2 justify-center items-center px-5 py-2 mt-6 border border-primary-500 rounded-lg">
             <h1 class="font-work-sans font-semibold text-base text-primary-500">See More</h1>
             <img src="..\assets\img\arrow-right.svg" alt="">
         </button>
     </div>
 
-    <h1>yes</h1>
+    <!-- outlets -->
+    <div class="flex flex-col bg-primary-50 bg-cover items-center justify-center my-[100px] bg-center gap-6 py-[80px]">
+        <!-- <img class="z-0 object-cover" src="..\assets\icons\circles.svg" alt=""> -->
+        <h1 class="font-lora font-semibold text-2xl text-neutral-900">Outlets</h1>
+        <h2 class="font-lora font-bold text-4xl ">We Are <span class="text-primary-500">Everywhere</span></h2>
+        <div class="flex flex-row items-center gap-6 pt-12">
+            <div class="flex flex-col bg-neutral-50 shadow-xl rounded-2xl">
+                <img src="..\assets\img\Rectangle 1.svg" alt="">
+                <div class="flex flex-col items-start p-6">
+                    <div class="flex flex-col item-start gap-y-6">
+                        <h1 class="font-lora font-semibold text-xl text-neutral-900">Bakso President Batanghari</h1>
+                        <div class="flex flex-col gap-y-4">
+                            <div class="flex flex-row font-normal text-base text-neutral-600 gap-3 items-start justify-start">
+                                <img class="pt-[1px] w-5" src="..\assets\icons\pin.svg" alt="">
+                                <p class="w-[300px]">Jl. Batanghari No.5, Rampal Celaket, Kec. Klojen, Kota Malang, Jawa Timur 65111</p>
+                            </div>
+                            <div class="flex flex-row font-normal text-base text-neutral-600 gap-3 items-start justify-start">
+                                <img class="pt-[1px] w-5" src="..\assets\icons\phone.svg" alt="">
+                                <p class="w-[300px]">0341 496 746 / 0812 5545 6020 (WA) </p>
+                            </div>
+                            <div class="flex flex-row font-normal text-base text-neutral-600 gap-3 items-start justify-start">
+                                <img class="pt-[1px] w-5" src="..\assets\icons\clock.svg" alt="">
+                                <p class="w-[300px]">08.00 - 21.30 WIB (Setiap Hari) </p>
+                            </div>
+                        </div>
+                        <div class="flex flex-row w-[180px] items-center justify-center bg-contain bg-primary-500 border border-primary-500 rounded-lg py-2 gap-2 hover:bg-primary-600 hover:duration-500">
+                            <img src="..\assets\icons\way.svg" alt="">
+                            <a class="font-medium text-base text-center text-neutral-50" href="">Petunjuk Arah</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="flex flex-col bg-neutral-50 shadow-xl rounded-2xl">
+                <img src="..\assets\img\Rectangle 1.svg" alt="">
+                <div class="flex flex-col items-start p-6">
+                    <div class="flex flex-col item-start gap-y-6">
+                        <h1 class="font-lora font-semibold text-xl text-neutral-900">Bakso President Pulosari</h1>
+                        <div class="flex flex-col gap-y-4">
+                            <div class="flex flex-row font-normal text-base text-neutral-600 gap-3 items-start justify-start">
+                                <img class="pt-[1px] w-5" src="..\assets\icons\pin.svg" alt="">
+                                <p class="w-[300px]">Jl. Batanghari No.5, Rampal Celaket, Kec. Klojen, Kota Malang, Jawa Timur 65111</p>
+                            </div>
+                            <div class="flex flex-row font-normal text-base text-neutral-600 gap-3 items-start justify-start">
+                                <img class="pt-[1px] w-5" src="..\assets\icons\phone.svg" alt="">
+                                <p class="w-[300px]">0341 496 746 / 0812 5545 6020 (WA) </p>
+                            </div>
+                            <div class="flex flex-row font-normal text-base text-neutral-600 gap-3 items-start justify-start">
+                                <img class="pt-[1px] w-5" src="..\assets\icons\clock.svg" alt="">
+                                <p class="w-[300px]">08.00 - 21.30 WIB (Setiap Hari) </p>
+                            </div>
+                        </div>
+                        <div class="flex flex-row w-[180px] items-center justify-center bg-contain bg-primary-500 border border-primary-500 rounded-lg py-2 gap-2 hover:bg-primary-600 hover:duration-500">
+                            <img src="..\assets\icons\way.svg" alt="">
+                            <a class="font-medium text-base text-center text-neutral-50" href="">Petunjuk Arah</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="flex flex-col bg-neutral-50 shadow-xl rounded-2xl">
+                <img src="..\assets\img\Rectangle 1.svg" alt="">
+                <div class="flex flex-col items-start p-6">
+                    <div class="flex flex-col item-start gap-y-6">
+                        <h1 class="font-lora font-semibold text-xl text-neutral-900">Bakso President Malang 1</h1>
+                        <div class="flex flex-col gap-y-4">
+                            <div class="flex flex-row font-normal text-base text-neutral-600 gap-3 items-start justify-start">
+                                <img class="pt-[1px] w-5" src="..\assets\icons\pin.svg" alt="">
+                                <p class="w-[300px]">Jl. Batanghari No.5, Rampal Celaket, Kec. Klojen, Kota Malang, Jawa Timur 65111</p>
+                            </div>
+                            <div class="flex flex-row font-normal text-base text-neutral-600 gap-3 items-start justify-start">
+                                <img class="pt-[1px] w-5" src="..\assets\icons\phone.svg" alt="">
+                                <p class="w-[300px]">0341 496 746 / 0812 5545 6020 (WA) </p>
+                            </div>
+                            <div class="flex flex-row font-normal text-base text-neutral-600 gap-3 items-start justify-start">
+                                <img class="pt-[1px] w-5" src="..\assets\icons\clock.svg" alt="">
+                                <p class="w-[300px]">08.00 - 21.30 WIB (Setiap Hari) </p>
+                            </div>
+                        </div>
+                        <div class="flex flex-row w-[180px] items-center justify-center bg-contain bg-primary-500 border border-primary-500 rounded-lg py-2 gap-2 hover:bg-primary-600 hover:duration-500">
+                            <img src="..\assets\icons\way.svg" alt="">
+                            <a class="font-medium text-base text-center text-neutral-50" href="">Petunjuk Arah</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     @include('partials.footer')
 </body>
 </html>
