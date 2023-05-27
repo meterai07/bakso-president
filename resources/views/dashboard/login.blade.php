b6a1c06017a54fb888e4e8378a106ae369fa6144
Tengku rafi <t.mrr39@gmail.com>
2023-05-27 21:35:16 +0700

    modified:   resources/views/dashboard/login.blade.php

--- resources/views/dashboard/login.blade.php
+++ resources/views/dashboard/login.blade.php
@@ -12,6 +12,13 @@
                 <h1 class="text-3xl font-bold text-gray-900">Login Admin</h1>
                 <p class="mt-2 text-sm text-gray-600">Masukkan email dan password anda</p>
             </div>
+
+            @if(session('error'))
+            <div class="flex items-center justify-center w-full p-4 mb-4 text-red-500 bg-red-100 rounded-lg">
+                {{ session('error') }}
+            </div>
+            @endif
+
             <div class="flex flex-col w-full p-6 space-y-6">
                 <input type="email" name="email" id="email" placeholder="Email" class="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-200 focus:border-transparent">
                 <input type="password" name="password" id="password" placeholder="Password" class="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-200 focus:border-transparent">
