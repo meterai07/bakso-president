5684983046c304791b5b7164c2a087cdf4b0831b
canninoal <canninoalby@gmail.com>
2023-06-05 10:59:19 +0700

    modified:   resources/views/dashboard/components/navbar.blade.php 	modified:   resources/views/dashboard/menu/menu-update.blade.php

--- resources/views/dashboard/components/navbar.blade.php
+++ resources/views/dashboard/components/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav class="flex w-full py-4 items-center justify-between duration-500 bg-neutral-50 z-50 px-8">
-        <a href="/"><img src="../assets/icons/Logo-Bakso.png" alt="Logo" class="h-12 w-auto"></a>
+        <a href="/"><img src="../../../assets/icons/Logo-Bakso.png" alt="Logo" class="h-12 w-auto"></a>
         <div class="flex items-center justify-between gap-8 pr-[100px]">
             <a class="text-base font-medium text-neutral-900 hover:text-primary-500 hover:duration-100" href="/admin/menu">Menu</a>
             <a class="text-base font-medium text-neutral-900 hover:text-primary-500 hover:duration-100" href="/admin/categories">Categories</a>
--- resources/views/dashboard/menu/menu-update.blade.php
+++ resources/views/dashboard/menu/menu-update.blade.php
@@ -3,48 +3,47 @@
 @section('title', ' Menu Update')
 
 @section('content')
-<div class="h-screen bg-primary-50 items-center">
-  <div class="flex flex-col items-center pt-12">
-    <h1 class="p-4">Halaman Update</h1>
-      <!-- {{-- <form action="/admin/menu/update/{{ $menu->id }}" method="post" enctype="multipart/form-data">
-          @csrf
-          <input type="text" name="name" id="name" placeholder="Nama Menu" value="{{ $menu->name }}">
-          <input type="text" name="description" id="description" placeholder="Deskripsi Menu" value="{{ $menu->description }}">
-          <input type="number" name="price" id="price" placeholder="Harga Menu" value="{{ $menu->price }}">
-          <img src="{{ asset('storage/' . $menu->image) }}" alt="{{ $menu->name }}" width="200">
-          <input type="file" name="image" id="image" placeholder="Gambar Menu">
-          <button type="submit">Update Menu</button>
-      </form> --}} -->
-      <form action="/admin/menu/update/{{ $menu->id }}" method="post" enctype="multipart/form-data" class="w-full max-w-sm mx-auto">
-          @csrf
-          <div class="mb-4">
-            <label for="name" class="block text-gray-700 text-sm font-bold mb-2">Nama Menu</label>
-            <input type="text" name="name" id="name" placeholder="Nama Menu" value="{{ $menu->name }}" class="appearance-none border border-gray-300 rounded w-full py-2 px-3 leading-tight focus:outline-none focus:border-blue-500">
-          </div>
-          <div class="mb-4">
-            @foreach($categories as $category)
-              <input type="radio" name="category_id" id="category_id" value="{{ $category->id }}" {{ $menu->category_id == $category->id ? 'checked' : '' }}>
-              <label for="category_id" class="mr-2">{{ $category->name }}</label>
-            @endforeach
-          </div>
-          <div class="mb-4">
-            <label for="description" class="block text-gray-700 text-sm font-bold mb-2">Deskripsi Menu</label>
-            <input type="text" name="description" id="description" placeholder="Deskripsi Menu" value="{{ $menu->description }}" class="appearance-none border border-gray-300 rounded w-full py-2 px-3 leading-tight focus:outline-none focus:border-blue-500">
-          </div>
-          <div class="mb-4">
-            <label for="price" class="block text-gray-700 text-sm font-bold mb-2">Harga Menu</label>
-            <input type="number" name="price" id="price" placeholder="Harga Menu" value="{{ $menu->price }}" class="appearance-none border border-gray-300 rounded w-full py-2 px-3 leading-tight focus:outline-none focus:border-blue-500">
-          </div>
-          <div class="mb-4">
-            <img src="{{ asset('storage/' . $menu->image) }}" alt="{{ $menu->name }}" class="mb-2" width="200">
-            <label for="image" class="block text-gray-700 text-sm font-bold mb-2">Gambar Menu</label>
-            <input type="file" name="image" id="image" class="appearance-none border border-gray-300 rounded w-full py-2 px-3 leading-tight focus:outline-none focus:border-blue-500">
-          </div>
-          <div class="text-center">
-            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Update Menu</button>
-          </div>
-        </form>      
-  </div>
+
+<div class="h-screen bg-primary-50">
+  <a href="/admin/menu" class="flex text-blue-500 hover:text-blue-700 items-center ml-12 pt-4 gap-2 font-semibold w-8">
+    <img src="/assets/icons/chevron-left.svg" alt="">
+    Kembali
+  </a>
+  <div class="flex items-center justify-center">
+    <div class="flex flex-col bg-cover items-center justify-center w-[500px] h-[600px] py-4 gap-4 shadow-lg rounded-[50px]" style="background-image: url(/assets/img/login-bg.svg)">
+      <h1 class="text-3xl font-extrabold text-neutral-900 font-lora">Tambah Menu</h1>
+      <form action="/admin/menu/create" method="post" enctype="multipart/form-data" class="w-full max-w-sm mx-auto">
+        @csrf
+        <div class="mb-4">
+          <label for="name" class="block text-gray-700 text-sm font-bold mb-2">Nama Menu</label>
+          <input type="text" name="name" id="name" placeholder="Nama Menu" value="{{ $menu->name }}" class="px-4 py-2 border border-neutral-200 rounded-lg shadow-md w-full focus:outline-none focus:ring-2 focus:ring-gray-200 focus:border-transparent focus:duration-500">
+        </div>
+        <div class="flex flex-row mb-4 gap-2 items-center">
+          @foreach($categories as $category)
+          <label class="flex-row items-center text-gray-700 text-sm font-bold">
+            <input type="radio" name="category_id" id="category_id" value="{{ $category->id }}" class="" {{ $menu->category_id == $category->id ? 'checked' : '' }}>
+            {{ $category->name }}
+          </label>
+          @endforeach
+        </div>
+        <div class="mb-4">
+          <label for="description" class="block text-gray-700 text-sm font-bold mb-2">Deskripsi Menu</label>
+          <input type="text" name="description" id="description" placeholder="Deskripsi Menu" value="{{ $menu->description }}" class="px-4 py-2 border border-neutral-200 rounded-lg shadow-md w-full focus:outline-none focus:ring-2 focus:ring-gray-200 focus:border-transparent focus:duration-500">
+        </div>
+        <div class="mb-4">
+          <label for="price" class="block text-gray-700 text-sm font-bold mb-2">Harga Menu</label>
+          <input type="number" name="price" id="price" placeholder="Harga Menu" value="{{ $menu->price }}" class="px-4 py-2 border border-neutral-200 rounded-lg shadow-md w-full focus:outline-none focus:ring-2 focus:ring-gray-200 focus:border-transparent focus:duration-500">
+        </div>
+        <div class="mb-4">
+          <img src="{{ asset('storage/' . $menu->image) }}" alt="{{ $menu->name }}" class="mb-2" width="200">
+          <label for="image" class="block text-gray-700 text-sm font-bold mb-2">Gambar Menu</label>
+          <input type="file" name="image" id="image" class="px-4 py-2 border border-neutral-200 rounded-lg shadow-md w-full focus:outline-none focus:ring-2 focus:ring-gray-200 focus:border-transparent focus:duration-500">
+        </div>
+        <div class="text-center ml-auto mr-auto bg-primary-500 mt-4 rounded-lg shadow-lg w-fit text-neutral-50 hover:bg-primary-600 hover:duration-500">
+          <button type="submit" class=" bg-blue-500 hover:bg-blue-700 text-neutral-900 py-2 px-4 rounded focus:outline-none focus:shadow-outline">Confirm</button>
+        </div>
+      </form>  
+    </div>
+  </div>    
 </div>
-    
 @endsection
